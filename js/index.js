8273cfec5fabeb2eb4ffb31722f6104d3cf35ddf
Rajesh <rajesh.facile@outlook.com>
2021-03-06 03:42:01 -0500

    ternary operator using javascript

--- js/index.js
+++ js/index.js
@@ -1,9 +1,4 @@
 
-//strict equality
-console.log(1 === 1);
-console.log('1' === 1);
-
-//loose equality
-console.log(1 == 1);
-console.log('1' == 1);
-console.log(true == 1);
+let point = 110;
+let type = point > 100 ? 'gold' : 'silver';
+console.log(type);
