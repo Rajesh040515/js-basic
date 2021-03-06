fe15c04319179d68cba9173e48bbc0041a5aff5f
Rajesh <rajesh.facile@outlook.com>
2021-03-06 03:34:20 -0500

    equality operator using javascript

--- js/index.js
+++ js/index.js
@@ -1,12 +1,9 @@
 
-let x = 1;
+//strict equality
+console.log(1 === 1);
+console.log('1' === 1);
 
-console.log(x >= 0);
-console.log(x >= 1);
-console.log(x < 1);
-console.log(x <= 1);
-
-
-//equality operarotor
-console.log(x === 1);
-console.log(x !== 1);
+//loose equality
+console.log(1 == 1);
+console.log('1' == 1);
+console.log(true == 1);
