3ea59dab22ff1f156d350b247aa9da2d29764593
Rajesh <rajesh.facile@outlook.com>
2021-03-06 05:41:41 -0500

    exercise of variable swapping and operator

--- js/index.js
+++ js/index.js
@@ -1,3 +1,14 @@
-let x = (2 + 3) * 5;
-console.log(x);
 
+//logical operator exercise
+//let x = 10;
+//let y = (x > 5) && (x < 15);
+//console.log(y);
+
+let a = 'red';
+let b = 'blue';
+let c = a;
+a = b;
+b = c;
+
+console.log(a);
+console.log(b);
