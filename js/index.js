f25770bfda9dcfae68d28e5d352c9e189e4c97da
Rajesh <rajesh.facile@outlook.com>
2021-03-09 02:42:19 -0500

    prime number of javascript

--- js/index.js
+++ js/index.js
@@ -1,40 +1,26 @@
+showprimes(50);
+function showprimes(limit) {
 
-const marks = [80, 80];
-console.log(calcutegrade(9));
-
-
-function calcutegrade(marks) {
-
-    const average = calculateAverage(marks);
-
-    if (average >= 90 && average <= 100) {
-        return 'A';
+    for (let number = 2; number <= limit; number++) {
+        if (isprime(number))
+            console.log(number);
     }
 
-    if (average >= 80 && average <= 89) {
-        return 'b';
-    }
-    if (average >= 70 && average <= 79) {
-        return 'c';
-    }
-    if (average >= 60 && average <= 69) {
-        return 'd';
-    }
-    if (average >= 1 && average <= 59) {
-        return 'f';
-    }
-    else {
-        return 'faile';
-    }
+    function isprime(number) {
 
 
+        for (let factor = 2; factor < number; factor++)
 
-    function calculateAverage(array) {
-
-        let sum = 0;
-        for (let value of array)
-            sum += value;
-        return sum / array.length;
+            if (number % factor === 0)
+                return false;
+        return true;
     }
+
 }
+
+
+
+
+
+
 
