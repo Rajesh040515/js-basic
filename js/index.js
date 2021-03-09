4712278517f90ef11b0cada7a9688ba02f3ba81d
Rajesh <rajesh.facile@outlook.com>
2021-03-09 01:54:37 -0500

    grade exercise of javascript

--- js/index.js
+++ js/index.js
@@ -1,17 +1,40 @@
 
+const marks = [80, 80];
+console.log(calcutegrade(9));
 
 
-console.log(sum(10));
-function sum(limit) {
+function calcutegrade(marks) {
 
-    let sum = 0;
+    const average = calculateAverage(marks);
 
-    for (let i = 0; i <= limit; i++) {
-        if (i % 3 === 0 || i % 5 === 0)
-            sum += i;
-        return sum;
+    if (average >= 90 && average <= 100) {
+        return 'A';
+    }
+
+    if (average >= 80 && average <= 89) {
+        return 'b';
+    }
+    if (average >= 70 && average <= 79) {
+        return 'c';
+    }
+    if (average >= 60 && average <= 69) {
+        return 'd';
+    }
+    if (average >= 1 && average <= 59) {
+        return 'f';
+    }
+    else {
+        return 'faile';
+    }
+
+
+
+    function calculateAverage(array) {
+
+        let sum = 0;
+        for (let value of array)
+            sum += value;
+        return sum / array.length;
     }
 }
-
-
 
