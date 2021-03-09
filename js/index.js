b12ff226bff159e6f1c24ded5e8e8664d3f096fd
Rajesh <rajesh.facile@outlook.com>
2021-03-09 00:01:36 -0500

    for of loop using javascript

--- js/index.js
+++ js/index.js
@@ -1,16 +1,8 @@
-const person = {
 
-    name: 'rajesh',
-    age: 21
-};
-
-for (let key in person) {
-    console.log(key, person[key]);
-}
 
 const colors = ['red', 'green', 'blue'];
-for (let r in colors) {
+for (let r of colors) {
 
-    console.log(r, colors[r]);
+    console.log(r);
 
 }
