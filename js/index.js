c5f163adbe7a5ee25b7d04e2cfb4705e1f5599a7
Rajesh <rajesh.facile@outlook.com>
2021-03-10 04:37:58 -0500

    function declaration

--- js/index.js
+++ js/index.js
@@ -1,15 +1,10 @@
-const movie = [
+function walk() {
+    console.log('walk');
 
+}
+let run = function () {
 
-    { title: 'a', year: '2018', rating: '4' },
-    { title: 'b', year: '2018', rating: '4' },
-    { title: 'c', year: '2018', rating: '3' },
-    { title: 'd', year: '2018', rating: '4.5' },
+    console.log('run')
 
-]
-const title = movie
-    .filter(m => m.year === 2018 && m.rating >= 3)
-    .sort((a, b) => a.rating = b.rating)
-    .reverse()
-    .map(m => m.title)
-console.log(title);
+};
+run();
