d1ab5dba1324056f8f56fe97711551fcf3aeca3b
Rajesh <rajesh.facile@outlook.com>
2021-03-09 23:27:26 -0500

    spread operator combination array of js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
-const numbers = [1, 2, 3, 4];
+const numbers = [1, 2, 3];
 const numbers2 = [4, 5, 6];
 
-const result = numbers.concat(numbers2);
-const slice = result.slice(2, 6);
-console.log(result);
-console.log(slice);
+
+const combined = [...numbers, ...numbers2];
+const copy = [...combined];
+console.log(copy);
