3b0ced38c0a83b2b35046b29558bbc3ce9d7c165
Rajesh <rajesh.facile@outlook.com>
2021-03-09 23:19:16 -0500

    combine and slice array of js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
 const numbers = [1, 2, 3, 4];
+const numbers2 = [4, 5, 6];
 
-const first = numbers.shift();
-
-console.log(first);
+const result = numbers.concat(numbers2);
+const slice = result.slice(2, 6);
+console.log(result);
+console.log(slice);
