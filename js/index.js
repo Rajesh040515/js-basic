a4d76d5da3692d9464cca305e69854403159e0fd
Rajesh <rajesh.facile@outlook.com>
2021-03-10 01:18:01 -0500

    testing element of js

--- js/index.js
+++ js/index.js
@@ -1,20 +1,8 @@
-//const number = [3, 1, 2];
-//number.sort();
-//console.log(number);
+const number = [3, -1, 1, 2];
+const positive = number.some(function (value) {
 
-//number.reverse();
-//console.log(number);
+    return value >= 0;
 
-const courses = [
-
-    { id: 1, name: 'node' },
-    { id: 2, name: 'java' },
-];
-
-courses.sort(function (a, b) {
-    if (a.name < b.name) return -1;
-    if (a.name > b.name) return 1;
-    return 0;
 });
-console.log(courses);
+console.log(positive);
 
