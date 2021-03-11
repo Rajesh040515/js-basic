52e61696d9841a4be0bd380014dab73bd5416dfe
Rajesh <rajesh.facile@outlook.com>
2021-03-11 04:18:10 -0500

    exercise error handling

--- js/index.js
+++ js/index.js
@@ -1,15 +1,28 @@
-const number = [1, 2, 3, 4];
+try {
+    const number = [1, 2, 3, 4];
+    const count = countoccurences(number, 1);
+    console.log(count);
+}
+catch (e) {
+    console.log(e.message);
+}
+const countoccurences = count(array, searchelement)
+{
+    if (!Array.isArray(array))
+        throw new Error('invalide');
+    return array.reduce((accumulator, current) => {
+
+        const occurence = (current === searchelement) ? 1 : 0;
+        return accumulator + occurence;
+    }, 0);
+
+}
+
+/*const num1 = [5, 6, 8];
 
 
-const ans = number.map(n => {
-    return {
-        id: n,
-        name: 'Apple',
-        price: 50,
-        color: 'Red'
 
-    };
-});
-console.log(ans);
-
-
+const add = num1.filter(num1 => num1 > 5)
+console.log(add);
+console.log(num1);
+*/
